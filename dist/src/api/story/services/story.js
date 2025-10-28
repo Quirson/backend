24b7519369cb6a28b1c80550d3f5e0314c50ac7a
Ngale Junior <quirsonngale@gmail.com>
2025-10-28 21:18:05 +0200

    Actualizacao
    28 /10

--- dist/src/api/story/services/story.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * story service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::story.story');
